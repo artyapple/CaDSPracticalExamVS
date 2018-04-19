ddfc858578ac86d8495a6fcbe1f88927fc22d3d8
Artur Iablokov <artur.iablokov@haw-hamburg.de>
2018-04-19 18:54:35 +0200

    Dummy Server Application added

--- src/cads/impl/app/server/ServerApplication.java
+++ src/cads/impl/app/server/ServerApplication.java
@@ -1,21 +1,43 @@
 package cads.impl.app.server;
 
+import java.io.IOException;
+
 import org.cads.ev3.middleware.CaDSEV3RobotHAL;
 import org.cads.ev3.middleware.CaDSEV3RobotType;
 import org.cads.ev3.middleware.hal.CaDSEV3Robot;
 import org.cads.ev3.middleware.hal.ICaDSEV3RobotFeedBackListener;
 import org.cads.ev3.middleware.hal.ICaDSEV3RobotStatusListener;
 
-public class ServerApplication {
-	
-	private static CaDSEV3RobotHAL robot = null;
-	
-	public static void main(String[] args) {
-		
-		ICaDSEV3RobotStatusListener statusListener;
-		ICaDSEV3RobotFeedBackListener feedbackListener;
-		robot = new CaDSEV3RobotHAL(CaDSEV3RobotType.SIMULATION, statusListener, feedbackListener);
-	}
-	
-}
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 
+import cads.impl.app.server.listener.FeedBackListener;
+import cads.impl.app.server.listener.RobotStatusListener;
+import cads.impl.app.server.listener.RobotStatusListener.ValueToObserve;
+import cads.impl.app.server.listener.Status;
+import cads.impl.hal.IVertikalMotor;
+import cads.impl.hal.server.HorizontalMotor;
+import cads.impl.hal.server.VertikalMotor;
+import cads.impl.mom.MarshallingService;
+
+public class ServerApplication {
+
+	public static void main(String[] args) throws JsonParseException, JsonMappingException, IOException {
+
+		 RobotStatusListener statusListener = new RobotStatusListener();
+		 CaDSEV3RobotHAL robot = CaDSEV3RobotHAL.createInstance(CaDSEV3RobotType.SIMULATION, statusListener, new FeedBackListener());
+		 
+		 VertikalMotor vmotor = new VertikalMotor();
+		 statusListener.subscribe(ValueToObserve.VERTIKAL, vmotor);
+		 HorizontalMotor hmotor = new HorizontalMotor();
+		 statusListener.subscribe(ValueToObserve.HORIZONTAL, hmotor);
+		 
+		 vmotor.move(50);
+		 vmotor.move(0);
+		 vmotor.move(100);
+		 hmotor.move(100);
+		 vmotor.move(0);
+		 hmotor.move(50);
+
+	}
+}
