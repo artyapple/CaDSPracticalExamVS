8a57d055c0fb2f39f13b62b18e260d3f31ab612f
Artur Iablokov <artur.iablokov@haw-hamburg.de>
2018-04-19 18:52:09 +0200

    Dummy Feedbacklistener

---
+++ src/cads/impl/app/server/listener/FeedBackListener.java
@@ -0,0 +1,14 @@
+package cads.impl.app.server.listener;
+
+import org.cads.ev3.middleware.hal.ICaDSEV3RobotFeedBackListener;
+import org.json.simple.JSONObject;
+
+public class FeedBackListener implements ICaDSEV3RobotFeedBackListener {
+
+	@Override
+	public void giveFeedbackByJSonTo(JSONObject arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
