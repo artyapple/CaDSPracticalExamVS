3f4e83327631ff53ebc3ede9ff23dba5fe9b2c0b
Artur Iablokov <artur.iablokov@haw-hamburg.de>
2018-04-26 18:17:11 +0200

    Client and Server Application (Main/Setup) changed

--- src/cads/impl/app/client/ClientApplication.java
+++ src/cads/impl/app/client/ClientApplication.java
@@ -13,63 +13,64 @@ import org.cads.ev3.rmi.generated.cadSRMIInterface.IIDLCaDSEV3RMIUltraSonic;
 
 import cads.impl.app.client.gui.HorizontalMovingGuiController;
 import cads.impl.app.client.gui.VerticalMovingGuiController;
+import cads.impl.mom.ClientMiddleware;
 import cads.impl.mom.Message;
 import cads.impl.mom.MessageBuffer;
-import cads.impl.mom.MessageHandler;
+import cads.impl.mom.Middleware;
 import cads.impl.os.Client;
 import cads.impl.os.Server;
 import cads.impl.os.UDPClient;
 import cads.impl.os.UDPServer;
 
 public class ClientApplication {
-	
-	//static CaDSRobotGUISwing gui;
 
-	
-	
-	
-	public static void main(String[] args) throws InstantiationException, IllegalAccessException, SocketException, UnknownHostException {
-		
-		//VERTIKAL SERVICE and VERTIKAL MESSAGE HANDLER SETUP
-		//1)create vertikal buffer
+	// static CaDSRobotGUISwing gui;
+
+	public static void main(String[] args)
+			throws InstantiationException, IllegalAccessException, SocketException, UnknownHostException {
+
+		// VERTIKAL SERVICE and VERTIKAL MESSAGE HANDLER SETUP
+		// 1)create vertikal buffer
 		MessageBuffer<Message> buffer = new MessageBuffer<>();
 
-		//2)create vertikal gui controller 
-		IIDLCaDSEV3RMIMoveVertical vertikal = new VerticalMovingGuiController(buffer);		
+		// 2)create vertikal gui controller
+		IIDLCaDSEV3RMIMoveVertical vertikal = new VerticalMovingGuiController(buffer);
 
-		//3)NETWORK SETUP for vertikal!
+		// 3)NETWORK SETUP for vertikal!
 		// servers ip and port ?! klaeren
-		Client<String> client= new UDPClient(InetAddress.getByName("localhost"), 8010);
-		//  port ?
-		Server<String> server = new UDPServer(8010);		
+		Client<String> client = new UDPClient(InetAddress.getByName("localhost"), 8010);
+		// port ?
 		// NETWORK SETUP END for vertikal
-		
-		//4) Message handler for vertikal
-		MessageHandler vertikalMom = new MessageHandler(buffer, client, server);
-		//5)vertikal ready
-		
-		//HERE other handlers and other services
-		
-		//HORIZONTAL SERVICE and HORIZONTAL MESSAGE HANDLER SETUP
+
+		// 4) Message handler for vertikal
+		Middleware vertikalMom = new ClientMiddleware(buffer, client);
+		// 5)vertikal ready
+
+		// HERE other handlers and other services
+
+		// HORIZONTAL SERVICE and HORIZONTAL MESSAGE HANDLER SETUP
 		// horizontal buffer
 		MessageBuffer<Message> horizontalMessageBuffer = new MessageBuffer<>();
 		// horizontal gui controller
 		IIDLCaDSEV3RMIMoveHorizontal horizontal = new HorizontalMovingGuiController(horizontalMessageBuffer);
 
-		//3)NETWORK SETUP for horizontal!
+		// 3)NETWORK SETUP for horizontal!
 		// servers ip and port ?! klaeren
-		Client<String> clientH= new UDPClient(InetAddress.getByName("localhost"), 8011);
-		//  port ?
-		Server<String> serverH = new UDPServer(8011);		
+		Client<String> clientH = new UDPClient(InetAddress.getByName("localhost"), 8011);
+		// port ?
+		// Server<String> serverH = new UDPServer(8011);
 		// NETWORK SETUP END for horizontal
+
+		// 4) Message handler for horizontal
+		Middleware horizontalMom = new ClientMiddleware(horizontalMessageBuffer, clientH);
+		// 5)horizontal ready
+
+		CaDSRobotGUISwing gui = new CaDSRobotGUISwing(null, null, vertikal, horizontal, null);
+		// gui.start oder wie auch immer
 		
-		//4) Message handler for horizontal
-		MessageHandler horizontalMom = new MessageHandler(horizontalMessageBuffer, clientH, serverH);
-		//5)horizontal ready
-		
-		CaDSRobotGUISwing gui = new CaDSRobotGUISwing(null, null, vertikal , horizontal, null);
-		//gui.start oder wie auch immer
-	
+		new Thread(vertikalMom).start();
+		new Thread(horizontalMom).start();
+
 	}
 
 }
--- src/cads/impl/app/server/ServerApplication.java
+++ src/cads/impl/app/server/ServerApplication.java
@@ -1,43 +1,75 @@
 package cads.impl.app.server;
 
 import java.io.IOException;
+import java.net.InetAddress;
 
 import org.cads.ev3.middleware.CaDSEV3RobotHAL;
 import org.cads.ev3.middleware.CaDSEV3RobotType;
 import org.cads.ev3.middleware.hal.CaDSEV3Robot;
 import org.cads.ev3.middleware.hal.ICaDSEV3RobotFeedBackListener;
 import org.cads.ev3.middleware.hal.ICaDSEV3RobotStatusListener;
+import org.cads.ev3.rmi.generated.cadSRMIInterface.IIDLCaDSEV3RMIMoveVertical;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 
+import cads.impl.app.client.gui.VerticalMovingGuiController;
+import cads.impl.app.server.controller.HorizontalRobotController;
+import cads.impl.app.server.controller.RobotController;
+import cads.impl.app.server.controller.VertikalRobotController;
 import cads.impl.app.server.listener.FeedBackListener;
 import cads.impl.app.server.listener.RobotStatusListener;
 import cads.impl.app.server.listener.RobotStatusListener.ValueToObserve;
 import cads.impl.app.server.listener.Status;
 import cads.impl.hal.IVertikalMotor;
+import cads.impl.hal.server.GripperMotor;
 import cads.impl.hal.server.HorizontalMotor;
 import cads.impl.hal.server.VertikalMotor;
+import cads.impl.mom.ClientMiddleware;
+import cads.impl.mom.IMessage;
 import cads.impl.mom.MarshallingService;
+import cads.impl.mom.Message;
+import cads.impl.mom.MessageBuffer;
+import cads.impl.mom.Middleware;
+import cads.impl.mom.ServerMiddleware;
+import cads.impl.mom.Message.MsgType;
+import cads.impl.os.Client;
+import cads.impl.os.Server;
+import cads.impl.os.UDPClient;
+import cads.impl.os.UDPServer;
 
 public class ServerApplication {
 
 	public static void main(String[] args) throws JsonParseException, JsonMappingException, IOException {
 
-		 RobotStatusListener statusListener = new RobotStatusListener();
-		 CaDSEV3RobotHAL robot = CaDSEV3RobotHAL.createInstance(CaDSEV3RobotType.SIMULATION, statusListener, new FeedBackListener());
-		 
-		 VertikalMotor vmotor = new VertikalMotor();
-		 statusListener.subscribe(ValueToObserve.VERTIKAL, vmotor);
-		 HorizontalMotor hmotor = new HorizontalMotor();
-		 statusListener.subscribe(ValueToObserve.HORIZONTAL, hmotor);
-		 
-		 vmotor.move(50);
-		 vmotor.move(0);
-		 vmotor.move(100);
-		 hmotor.move(100);
-		 vmotor.move(0);
-		 hmotor.move(50);
+		RobotStatusListener statusListener = new RobotStatusListener();
+		CaDSEV3RobotHAL robot = CaDSEV3RobotHAL.createInstance(CaDSEV3RobotType.SIMULATION, statusListener,
+				new FeedBackListener());
 
+		VertikalMotor vmotor = new VertikalMotor();
+		statusListener.subscribe(ValueToObserve.VERTIKAL, vmotor);
+		HorizontalMotor hmotor = new HorizontalMotor();
+		statusListener.subscribe(ValueToObserve.HORIZONTAL, hmotor);
+		GripperMotor gmotor = new GripperMotor();
+		statusListener.subscribe(ValueToObserve.GRIPPER, gmotor);
+		
+		// vertikal
+		MessageBuffer<Message> vertikalBuffer = new MessageBuffer<>();
+		Server<String> vertikalServer = new UDPServer(8010);
+		Middleware vertikalMom = new ServerMiddleware(vertikalBuffer, vertikalServer);
+		RobotController vertikalRobotController = new VertikalRobotController(vertikalBuffer, vmotor);
+		
+		//horizontal
+		MessageBuffer<Message> horizontalBuffer = new MessageBuffer<>();
+		Server<String> horizontalServer = new UDPServer(8011);
+		Middleware horizontalMom = new ServerMiddleware(horizontalBuffer, horizontalServer);
+		RobotController horizontalRobotController = new HorizontalRobotController(horizontalBuffer, hmotor);
+		
+		// start threads
+		new Thread(vertikalMom).start();
+		new Thread(vertikalRobotController).start();
+		
+		new Thread(horizontalMom).start();
+		new Thread(horizontalRobotController).start();
 	}
 }
