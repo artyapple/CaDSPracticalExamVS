ab32cfbf69951312a3542267204189561c9583fc
Artur Iablokov <artur.iablokov@haw-hamburg.de>
2018-05-03 00:39:30 +0200

    Small changes in hal client side

--- src/cads/impl/hal/client/HorizontalMotor.java
+++ src/cads/impl/hal/client/HorizontalMotor.java
@@ -1,22 +1,22 @@
 package cads.impl.hal.client;
 
 import cads.impl.hal.IHorizontalMotor;
+import cads.impl.mom.IBuffer;
 import cads.impl.mom.Message;
 import cads.impl.mom.Message.MsgType;
-import cads.impl.mom.MessageBuffer;
 
 public class HorizontalMotor implements IHorizontalMotor {
 	
-	private MessageBuffer<Message> horizontalMessageBuffer;
+	private IBuffer<Message> horizontalMessageBuffer;
 
-	public HorizontalMotor(MessageBuffer<Message> horizontalMessageBuffer) {
+	public HorizontalMotor(IBuffer<Message> horizontalMessageBuffer) {
 		this.horizontalMessageBuffer = horizontalMessageBuffer;
 	}
 
 	@Override
 	public void move(int value) {
 		Message message = new Message(MsgType.HORIZONTAL, 1, value, 0);
-		horizontalMessageBuffer.addMessage(message);
+		horizontalMessageBuffer.add(message);
 	}
 
 }
--- src/cads/impl/hal/client/VertikalMotor.java
+++ src/cads/impl/hal/client/VertikalMotor.java
@@ -1,23 +1,22 @@
 package cads.impl.hal.client;
 
 import cads.impl.hal.IVertikalMotor;
-import cads.impl.mom.IMessage;
+import cads.impl.mom.IBuffer;
 import cads.impl.mom.Message;
 import cads.impl.mom.Message.MsgType;
-import cads.impl.mom.MessageBuffer;
 
 public class VertikalMotor implements IVertikalMotor {
 
-	private MessageBuffer<Message> buffer;
+	private IBuffer<Message> buffer;
 	
-	public VertikalMotor(MessageBuffer<Message> buffer){
+	public VertikalMotor(IBuffer<Message> buffer){
 		this.buffer = buffer;
 	}
 	
 	@Override
 	public void move(int value) {
 		Message msg = new Message(MsgType.VERTIKAL, 1, value, 0);
-		buffer.addMessage(msg);
+		buffer.add(msg);
 	}
 
 }
