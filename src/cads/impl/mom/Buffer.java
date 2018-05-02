3632e8c146388260964ce66cd1855855870ab7e9
Artur Iablokov <artur.iablokov@haw-hamburg.de>
2018-05-03 00:31:12 +0200

    Buffer remastered

---
+++ src/cads/impl/mom/Buffer.java
@@ -0,0 +1,86 @@
+package cads.impl.mom;
+
+import java.util.concurrent.LinkedBlockingDeque;
+
+public class Buffer<T> implements IBuffer<T> {
+
+	private static final int DEF_CAPACITY = 10;
+	
+	private final int capacity;
+	private LinkedBlockingDeque<T> deque;
+	private volatile boolean empty = true;
+	
+	/**
+	 * @param capacity
+	 */
+	public Buffer(int capacity) {
+		super();
+		this.capacity = capacity;
+		this.deque = new LinkedBlockingDeque<>(capacity);
+	}
+	public Buffer() {
+		this(DEF_CAPACITY);
+	}
+
+	@Override
+	public synchronized void add(T message) {
+
+		while (empty == false) { // wait till the buffer becomes empty
+			try {
+				wait();
+			} catch (InterruptedException e) {
+			}
+		}
+		
+		if(deque.size()==capacity){
+			deque.clear();
+		}
+				
+		if (message != null) {
+			deque.add(message);
+		}
+		empty = false;
+		notifyAll();
+	}
+
+	@Override
+	public synchronized T getLast() {
+		while (empty == true) { // wait till something appears in the buffer
+			try {
+				wait();
+			} catch (InterruptedException e) {
+			}
+		}
+		empty = true;
+		notifyAll();
+
+		return deque.pollFirst();
+
+	}
+
+	@Override
+	public synchronized T getFirstAndClear() {
+		while (empty == true) { // wait till something appears in the buffer
+			try {
+				wait();
+			} catch (InterruptedException e) {
+			}
+		}
+		empty = true;
+		notifyAll();
+
+		T element = deque.pollLast();
+		deque.clear();
+		return element;
+	}
+	
+	@Override
+	public synchronized boolean hasElements() {
+		return !(deque.isEmpty());
+	}
+	
+	@Override
+	public synchronized int size(){
+		return deque.size();
+	}
+}
--- src/cads/impl/mom/IBuffer.java
+++ src/cads/impl/mom/IBuffer.java
@@ -1,7 +1,30 @@
 package cads.impl.mom;
 
+
 public interface IBuffer<T> {
-	public void addMessage(T message);
-	public T getLastMessage();
-	public boolean hasMessages();
+	/**
+	 * adds element (according to fifo)
+	 * @param message
+	 */
+	public void add(T element);
+	/**
+	 * gets element (according to fifo)
+	 * @return
+	 */
+	public T getLast();
+	/**
+	 * gets element (according to lifo) and clears buffer
+	 * @return 
+	 */
+	public T getFirstAndClear();
+	/**
+	 * true if buffer has elements, otherwise false
+	 * @return
+	 */
+	public boolean hasElements();
+	/**
+	 * gets size of this buffer
+	 * @return
+	 */
+	public int size();
 }
